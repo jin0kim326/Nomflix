06dc9522309a8dabbccdad592cc46d5c17d2f67f
김진영 <53967482+jin0kim326@users.noreply.github.com>
2020-12-15 19:58:34 +0900

    #6.2 movie & tv presenter

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -15,8 +15,8 @@ export default () => (
             <Route path = "/" exact component={Home} />
             <Route path = "/tv" component={TV} />            
             <Route path = "/search" component={Search} />
-            <Route path="movie/:id" component={Detail} /> 
-            <Route path="show/:id" component={Detail} /> 
+            <Route path="/movie/:id" component={Detail} /> 
+            <Route path="/show/:id" component={Detail} /> 
             <Redirect from="*" to="/" />
             </Switch>
         </Router>
